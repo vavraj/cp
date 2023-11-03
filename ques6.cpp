414dcd897fcde090456fa2ce6bfaeead56ae6e3c
Vaibhav Raj <113601669+vavraj@users.noreply.github.com>
2023-11-03 16:41:02 +0000

    ques6.cpp

---
+++ ques6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+    int n;
+	cin>>n;
+	cout<<n/2+1<<endl;    
+}
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
